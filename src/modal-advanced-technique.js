58c9ffafe291bde2c862b0da83c35b72a92b72fe
Fernando Daciuk <f.daciuk@gmail.com>
2018-10-28 23:33:05 -0300

    Add space between paragraphs on modal

--- src/modal-advanced-technique.js
+++ src/modal-advanced-technique.js
@@ -50,25 +50,25 @@ function getTechniqueText (technique) {
   const techniques = {
     'Bi-Set': () => (
       <Fragment>
-        <Typography>
+        <Typography paragraph>
           Consiste em fazer dois exercícios do mesmo grupo muscular sem
           descanso entre. Após a execução dos dois se faz um descanso conforme na
           tabela de treino.
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Exemplo: Rosca direta + Rosca alternada 4x12 – Bi-Set
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Executa-se a rosca direta falhando perto de 12 repetições e sem nenhum
           tipo de descanso se executa a rosca alternada falhando perto de 12
           repetições. Se da o intervalo conforme tabela e se repete o processo até
           completar as séries programadas pra cada exercício.
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Se for muito ruim fazer os 2 exercícios em sequência por qualquer motivo,
           pode fazer separado, mas isso vai gerar mais tempo dentro da academia.
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Vídeo: <A href='https://youtu.be/KGZhG9LGWsU' />
         </Typography>
       </Fragment>
@@ -76,24 +76,24 @@ function getTechniqueText (technique) {
 
     'Drop-Set': () => (
       <Fragment>
-        <Typography>
+        <Typography paragraph>
           Apenas executado na última série. Ao falhar se abaixa o peso em cerca de
           20 a 30% e se leva até a falha novamente, não importando quantas
           repetições forem necessárias pra isso acontecer. Se for "Drop-Set 3x" se
           executa três reduções de carga seguidas e levando até a falha pra cada
           redução sem nenhum tipo de descanso.
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Exemplo: Rosca direta 4x12 – Drop-Set 3x
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Executam-se todas as séries normalmente, o drop-set será aplicado
           apenas na última série. No final da quarta série se abaixa 20% do peso e
           executa até a falha. Abaixa mais 20% de peso e novamente leva-se até a
           falha. Abaixa mais 20% e novamente até a falha. Sem nenhum tipo de
           intervalo entre essas séries.
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Vídeo: <A href='https://youtu.be/r4rxW8sqpNo' />
         </Typography>
       </Fragment>
@@ -101,7 +101,7 @@ function getTechniqueText (technique) {
 
     'Rest \'n\' Pause': () => (
       <Fragment>
-        <Typography>
+        <Typography paragraph>
           Aplicado apenas na última série. Ao se falhar na última série se da um
           descanso de 5 a 15 segundos e se executa novamente, com o mesmo peso,
           até a falha. Caso esteja 3x na tabela se executa essa estratégia 3x seguidas
@@ -112,26 +112,25 @@ function getTechniqueText (technique) {
           (geralmente em exercícios de músculos pequenos – bíceps, tríceps, etc.),
           espera-se 5 segundos.
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Exemplo: Rosca direta 4x12 – Rest ‘n’ Pause 3x.
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Executam-se quatro séries normais. Ao falhar na quarta série se descansa
           de 5 a 15 segundos (pode largar os pesos) e tenta fazer mais repetições
           sem abaixar a carga. Atingindo a falha novamente se descansa mais 5 a 15
           segundos e repete o processo. Repete-se essa sequência mais uma vez,
           totalizando 3x.
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Vídeo: <A href='https://youtu.be/30PA_tBg-_8' />
         </Typography>
-
       </Fragment>
     ),
 
     'FST-7': () => (
       <Fragment>
-        <Typography>
+        <Typography paragraph>
           Se executa 7 séries com as repetições estipuladas na tabela com intervalo
           de 30 segundos entre as séries. Durante esse intervalo é feito um
           alongamento dos músculos que estão sendo trabalhados. Essa técnica
@@ -141,10 +140,10 @@ function getTechniqueText (technique) {
           carga utilizada é sempre a máxima, mas pode ser abaixada pra ficar
           dentro das repetições alvos, afinal o cansaço será bem alto.
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Vídeo: <A href='https://youtu.be/D4bBRKeJUjU' />
         </Typography>
-        <Typography>
+        <Typography paragraph>
           Os alongamentos podem ser feitos 15s de cada lado. Clique no músculo
           pra ver o alongamento: {' '}
           <A href='http://www.malhandocerto.com/wp-content/uploads/2013/08/alongamento-peitoral.jpg'>Peitoral</A>,
@@ -164,7 +163,7 @@ function getTechniqueText (technique) {
     ),
 
     'GVT': () => (
-      <Typography>
+      <Typography paragraph>
         O GVT é uma técnica de séries massivas para desenvolvimento muscular.
         Executa-se 10 séries de um determinado exercício com descanso de 60 a
         90 segundos (sempre tentando ficar em 60s). A carga utilizada é sempre a
